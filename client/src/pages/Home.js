90410f92c7d63aa8c78be0ce8a2fabc0ac894a94
AllaSravan25 <109868428+AllaSravan25@users.noreply.github.com>
2024-10-05 04:41:19 +0530

    changed url

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ import "../pages/styles/home.css";
 import AttendanceCalendar from "../components/attendanceMontly";
 import QuickSection from "../components/quickSection";
 import RevenueChart from "../components/RevenueChart";
-const API_BASE_URL = 'https://react-app-server-rho.vercel.app';
+const API_BASE_URL = 'https://react-app-server-two.vercel.app';
 
 export default function Home() {
   const [employeeCount, setEmployeeCount] = useState(0);
