ccc1770f861580daef0c16bced80259ceadcd41d
AllaSravan25 <109868428+AllaSravan25@users.noreply.github.com>
2024-10-23 08:55:27 +0530

    moved erp-rsfire origin to first place because that is a if statement

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 
 
 
-const allowedOrigin = process.env.ALLOWED_ORIGIN || 'https://react-app-front-silk.vercel.app'|| 'https://erp-rsfire.vercel.app' || '*';
+const allowedOrigin = process.env.ALLOWED_ORIGIN || 'https://erp-rsfire.vercel.app' || 'https://react-app-front-silk.vercel.app'  || '*';
 
 app.use(cors({
   origin: allowedOrigin,
